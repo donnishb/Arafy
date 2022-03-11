541339fbc3ac36e2ed8e5a2d660d0bf5e43d93b0
donnishb <donnishmac@gmail.com>
2022-03-11 07:26:29 +0000

    Heroku

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,11 @@ mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
 });
 
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+  
 app.use('/api/users', userRouter);
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
